f8ec3c22038e323859e2ea35538576a6be891176
MikMik <mikmikyt1011@gmail.com>
2022-04-20 13:10:34 +0200

    rewrite, screen shift kinda works, overall better than last commit

--- flappybird.asm
+++ flappybird.asm
@@ -1,81 +1,72 @@
-PLAYERCLR:	equ %00001000 ;color of the player/bird
-PIPECLR:	equ %01110000 ;color of the pipe
-
-
+PLAYERCLR:	equ %00001000
+PIPECLR: 	equ %01110000
+BGCLR:		equ %00111000	
 	
-	org		32768 ; place game in the middle of the memory
+	org 32768
 
+main:
+	ld	hl, $5962	;address of the first block
+	call 	drawPlayer
+	call 	drawWholePipe
+	call 	shiftScreen
+	halt
+	call 	shiftScreen
+	halt
+	call 	shiftScreen
+	halt
+	call 	shiftScreen
+	halt
+	ret 
 
-	call drawPlayer
-	call drawPipe
-	call movePipes
-	call movePipes
-	call movePipes
-	call movePipes
-
-;--------------------------------------------------------------------------------
-; routine for drawing player/bird
+	;-------------------------------------
+; player draw routine
+; usage: pass address of the first player block, it will handle the rest 
 drawPlayer:
-	ld		hl, 22915 ; 22915 = 22528 + 12*32 + 3
-	ld 		(hl), PLAYERCLR ;color the first block
-	inc 	hl	;go to next one
-	ld 		(hl), PLAYERCLR	;color the second block
-	ld		bc, 31 ;put difference from second block first row to first block second row
-	add 	hl, bc
-	ld 		(hl), PLAYERCLR ;color the first block
-	inc 	hl	;go to next one
-	ld 		(hl), PLAYERCLR	;color the second block
+	ld 	(hl), PLAYERCLR		; color the block
+	ret 
+;----------------------------------------
+
+
+;------------------------------------------
+; pipe draw routine
+; it draws the pipe in the last 2 columns
+drawWholePipe:
+	ld	hl, $581E 	; 2nd last block in first row
+	ld 	b, 22 		; number of rows
+	call drawPipeRowLoop	
+	ret 
+
+drawPipeRowLoop:
+	ld 	(hl), PIPECLR 	; paint it in pipe color
+	ld 	de, 32 		; load number of blocks needed to get below the first block
+	add 	hl, de 		; jump to the block below the first block
+	djnz 	drawPipeRowLoop ; loop until whole pipe has been drawn
+	ret 
+;------------------------
+
+;------------------------
+; screen shift routine
+shiftScreen:
+	ld 	hl, $5801 	; load block which needs to be moved in the row
+	ld 	c, 24 		; number of rows
+	call	shiftScreenWholeLoop
+	ret 
+
+shiftScreenWholeLoop:
+	ld 	b, 31 		; load number of blocks in a row - 1
+	call	shiftScreenRowLoop
+	dec	c
+	jp	nz, shiftScreenWholeLoop
 	ret
 
-;--------------------------------------------------------------------------------
-; routine for drawing pipe
-drawPipe:
-	ld		hl, 22558 ; 22558 = 22528 + 30
-	ld		bc, 31 ; number of space blocks between pipes
-	ld 		a, 22 ; number of rows on the screen
-	call drawPipeLoop
-	ret
+shiftScreenRowLoop:
+	ld 	a, (hl)		; move the pipe info to accumulator so we can move it to the block left
+	dec	hl		; go one block left
+	ld 	(hl), a		; move the pipe from accumulator to the block left
+	inc	hl
+	inc	hl 		; move 2 blocks to the right
+	djnz shiftScreenRowLoop
+	dec	hl		; go to last block in the row
+	ld	(hl), BGCLR	; paint the block with the background color
 
-drawPipeLoop:
-	;halt 
-	ld 		(hl), PIPECLR ;color the first block
-	inc 	hl	;go to next one
-	ld 		(hl), PIPECLR	;color the second block
-	add 	hl, bc ; go to next row
-	dec 	a ; decrement number of rows
-	jp 		nz, drawPipeLoop ; repeat
-	ret
-
-;-----------------------------------------------
-; routine for moving pipe blocks
-movePipes:
-	ld 		hl, 22528 ; first block
-	ld 		bc, 704 ; amount of blocks to move
-	call movePipesLoop
-
-movePipesLoop:
-	
-	ld 		d, h ;save the address so we can move the pipe later
-	ld 		e, l ;save the address so we can move the pipe later
-	inc 	hl	;go to next one
-	ld 		a, (hl) ;get the block color
-	cp 		PIPECLR ;compare with the pipe color
-	jp 		nz, decreaseMovePipesCounter ;if it's not the pipe color, go to the next block
-	ld 		a, l ; save the value of l in a so we can do and operation later
-	and 	%00011111 ; mask the value of the position of pipe to check if it is in first block in a row
-	jp 		z, deletePipe ;if it's in the first block, delete it
-	ld 		a, (hl) ; move the pipe info to accumulator so we can move it to the previous block
-	ld 		(de), a ; move the pipe from accumulator to previous block
-	ld 		(hl), %00111000 ; reset current block
-	call 	decreaseMovePipesCounter ;if there are blocks to move, go to the next block
-	ret  
-	
-
-deletePipe:
-	ld 		(hl), %00111000 ;delete the pipe
-	jp 		decreaseMovePipesCounter ;go to the next block
-
-decreaseMovePipesCounter:
-	dec 	bc
-	jp 		nz, movePipesLoop ;if there are blocks to move, go to the next block
 	ret 
